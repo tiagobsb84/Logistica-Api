a93caee5b50dddf1a79323dca7e4c594150b3f3c
Tiago <tiagobsb84@gmail.com>
2021-05-30 14:50:43 -0300

    Fazendo integracao ao banco de dados MySql

--- src/main/java/com/logistica/api/controller/ClientController.java
+++ src/main/java/com/logistica/api/controller/ClientController.java
@@ -1,31 +1,68 @@
 package com.logistica.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.logistica.domain.model.Cliente;
+import com.logistica.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class ClientController {
 	
+	private ClienteRepository clienteRepository;
+	
 	@GetMapping("/cliente")
 	public List<Cliente> listar() {
+		return clienteRepository.findAll();  		
+	}
+	
+	@GetMapping("/cliente/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		return clienteRepository.findById(clienteId)
+			.map(ResponseEntity::ok)
+			.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping("/cliente")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("/cliente/{clienteId}")
+	public ResponseEntity<Cliente> atualizar(@RequestBody Cliente cliente,  @PathVariable Long clienteId){
 		
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Maria");
-		cliente1.setEmail("maria@gmail.com");
-		cliente1.setTelefone("11 99999-9999");
+		if(!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
 		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Angel");
-		cliente2.setEmail("angel@gmail.com");
-		cliente2.setTelefone("61 88888-8888");
+		cliente.setId(clienteId);
+		cliente = clienteRepository.save(cliente);
 		
-		return Arrays.asList(cliente1, cliente2);
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/cliente/{clienteId}")
+	public ResponseEntity<Void> excluir(@PathVariable Long clienteId){
+		if(!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		clienteRepository.deleteById(clienteId);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
---
+++ src/main/java/excepttionHandle/ApiExceptionHandle.java
@@ -0,0 +1,20 @@
+package excepttionHandle;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ApiExceptionHandle extends ResponseEntityExceptionHandler {
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		return handleExceptionInternal(ex, "dados Inválido", headers, status, request);
+	}
+}
